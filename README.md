0829ce415f8b1768f9edcdb3117a48f26041f659
Sumeet Lubal <slubal@amazon.com>
2020-10-10 15:42:41 -0500

    Fixes native acceleration

---
+++ README.md
@@ -0,0 +1,73 @@
+# react-native-flip-component
+
+### Installation
+`npm install @sumitlubal/react-native-flippable-card --save`
+
+### Instructions
+Pass a boolean, front component, and back component as props. The boolean will determine if the front or back component should be displayed.
+
+### Demo
+To see the code from this GIF, check out `example.js` in the repo on GitHub.
+![](./demo.gif)
+
+### Props
+Name | Required? | Description
+---- | ----------|--------
+isFlipped | true | Boolean that controls if the view is flipped.
+frontView | true | Component for the front view.
+backView | true | Component for the back view.
+scale | false | Controls if the view zooms in or out while flipping. Defaults to 0.8
+scaleDuration | false | Controls how quickly the scale changes. Defaults to 100
+frontPerspective | false | Controls the perception of depth for the front component. Defaults to 1000
+backPerspective | false | Controls the perception of depth for the back component. Defaults to 1000
+rotateDuration | false | Controls the duration of the rotation. Defaults to 300
+containerStyles | false | Styles for the flip-component's container `<View>`. Defaults to null
+frontStyles | false | Styles for the `<Animated.View>` that wraps the front component. Defaults to null
+backStyles | false | Styles for the `<Animated.View>` that wraps the back component. Defaults to null
+
+### Example
+
+```javascript
+import React, { Component } from 'react';
+import FlipComponent from 'react-native-flip-component';
+import { View, Button, Text } from 'react-native';
+
+class Example extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFlipped: false;
+    };
+  }
+  render() {
+    <View>
+      <FlipComponent
+        isFlipped={this.state.isFlipped}
+        frontView={
+          <View>
+            <Text style={{ textAlign: 'center' }}>
+              Front Side
+            </Text>
+          </View>
+        }
+        backView={
+          <View>
+            <Text style={{ textAlign: 'center' }}>
+              Back Side
+            </Text>
+          </View>
+        }
+      />
+      <Button
+        onPress={() => {
+          this.setState({ isFlipped: !this.state.isFlipped })
+        }}
+        title="Flip"
+      />
+    </View>
+  }
+}
+
+```
+
+This library is just mere extension of https://github.com/plmok61/react-native-flip-component
---
+++ index.js
@@ -0,0 +1,144 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View, Animated, Easing, ViewPropTypes, Platform } from 'react-native';
+
+class FlipComponent extends Component {
+  static propTypes = {
+    isFlipped: PropTypes.bool.isRequired,
+    frontView: PropTypes.element.isRequired,
+    backView: PropTypes.element.isRequired,
+    scale: PropTypes.number,
+    frontPerspective: PropTypes.number,
+    backPerspective: PropTypes.number,
+    scaleDuration: PropTypes.number,
+    rotateDuration: PropTypes.number,
+    containerStyles: ViewPropTypes.style,
+    frontStyles: ViewPropTypes.style,
+    backStyles: ViewPropTypes.style,
+  };
+
+  static defaultProps = {
+    scale: 0.8,
+    frontPerspective: 1000,
+    backPerspective: 1000,
+    scaleDuration: 100,
+    rotateDuration: 300,
+    containerStyles: null,
+    frontStyles: null,
+    backStyles: null,
+  };
+
+  constructor(props) {
+    super(props);
+    this.flip = this.flip.bind(this);
+    this.animatedScale = new Animated.Value(0);
+    this.animatedView = new Animated.Value(0);
+    this.frontInterpolate = this.animatedView.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '180deg'],
+    });
+    this.backInterpolate = this.animatedView.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['180deg', '360deg'],
+    });
+    this.scaleInterpolate = this.animatedScale.interpolate({
+      inputRange: [0, 1],
+      outputRange: [1, props.scale],
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.isFlipped !== this.props.isFlipped) {
+      this.flip();
+    }
+  }
+
+  flip() {
+    const toValue = this.props.isFlipped ? 1 : 0;
+
+    Animated.sequence([
+      Animated.timing(
+        this.animatedScale,
+        {
+          toValue: 1,
+          duration: this.props.scaleDuration,
+          easing: Easing.linear,
+          useNativeDriver: true
+        },
+      ),
+      Animated.timing(
+        this.animatedView,
+        {
+          toValue,
+          duration: this.props.rotateDuration,
+          easing: Easing.linear,
+          useNativeDriver: true
+        },
+      ),
+      Animated.timing(
+        this.animatedScale,
+        {
+          toValue: 0,
+          duration: this.props.scaleDuration,
+          easing: Easing.linear,
+          useNativeDriver: true
+        },
+      ),
+    ]).start();
+  }
+
+  render() {
+    const frontAnimatedStyle = { rotateY: this.frontInterpolate };
+    const backAnimatedStyle = { rotateY: this.backInterpolate };
+    const scaleAnimatedStyle = { scale: this.scaleInterpolate };
+    return (
+        <View style={this.props.containerStyles}>
+          <Animated.View
+            style={[
+              {
+                backfaceVisibility: 'hidden',
+                transform: [
+                  { perspective: this.props.frontPerspective },
+                  frontAnimatedStyle,
+                  scaleAnimatedStyle,
+                ],
+              },
+              Platform.select({
+                android: {
+                  opacity: this.animatedView.interpolate({ inputRange: [0, 0.5, 0.5], outputRange: [1, 1, 0] }),
+                },
+              }),
+              this.props.frontStyles,
+            ]}
+            pointerEvents={this.props.isFlipped ? 'none' : 'auto'}
+          >
+            {this.props.frontView}
+          </Animated.View>
+          <Animated.View
+            style={[
+              {
+                backfaceVisibility: 'hidden',
+                position: 'absolute',
+                transform: [
+                  { perspective: this.props.backPerspective },
+                  backAnimatedStyle,
+                  scaleAnimatedStyle,
+                ],
+              },
+              Platform.select({
+                android: {
+                  opacity: this.animatedView.interpolate({ inputRange: [0.5, 0.5, 1], outputRange: [0, 1, 1] }),
+                },
+              }),
+              this.props.backStyles,
+            ]}
+            pointerEvents={this.props.isFlipped ? 'auto' : 'none'}
+          >
+            {this.props.backView}
+          </Animated.View>
+        </View>
+    );
+  }
+}
+
+export default FlipComponent;
